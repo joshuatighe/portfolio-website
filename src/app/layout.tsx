bc3fff8cec290a746574421118f7c238892f67e2
Joshua Tighe <jtighe3@nd.edu>
2025-02-08 13:49:27 -0500

    implemented next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,17 @@
+import { Metadata } from 'next';
+import './globals.css'
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '500', '600']
+});
+
+export const metadata: Metadata = {
+  title: 'joshua tighe - portfolio website',
+  description: '',
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${inter.className} antialiased`}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
 export default function Home() {
   return (
-    <>
-      <h1>joshua tighe</h1>
-    </>
+    <div className="p-4">
+      <div>hello, i'm <p className="font-bold inline">joshua tighe</p></div>
+      <p>a sophomore CS student @ <a href="https://nd.edu" className="text-blue-700 hover:underline">ND</a> ☘️</p>
+      <div className="p-4">
+        <p>my links 🔗</p>
+        <p> 
+          - <a 
+            href="https://github.com/joshuatighe"
+            className="text-blue-700 hover:underline"
+            target="_blank">
+            github
+          </a>
+        </p>
+        <p>
+          - <a
+            href="https://www.linkedin.com/in/joshuatighe/"
+            className="text-blue-700 hover:underline"
+            target="_blank">
+            linkedin
+          </a>
+        </p>
+      </div>
+      <div className="p-4">
+        <p>my projects 📂</p>
+        <p>- 🚧</p>
+      </div>
+    </div>
   );
 }
